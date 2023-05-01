60afd1de866a91048d44d45747471832871915ce
ukuleledwarf <131986001+ukuleledwarf@users.noreply.github.com>
2023-05-01 20:31:57 +0800

    Add files via upload

--- render.js
+++ render.js
@@ -1,30 +1,42 @@
-const contentHTML = theory.map(section => `
-    <div class="section">
-        <div class="section__title">Раздел ${section.name}</div>
-        <div class="section__content">
-            ${section.themes.map(theme => `
-                <div class="section__theme">
-                    <div class="section__theme-title">${theme.title}</div>
-                    <div class="section__theme-content">
-                        ${theme.questions.map((question, qIndex) => `
-                            <div class="section__question" id="${question.id}">
-                                <div class="section__question-text">
-                                    <div class="section__question-text-number">${qIndex + 1}.&nbsp;</div>
-                                    ${question.text} ${question.siblings.map((sib, sInd) => `<span class="section__question-sibling" data-id="${sib}">Такой же ${sInd + 1}</span>`).join('')}
-                                </div>
-                                <div class="section__question-options">
-                                    ${question.options.map(option => `
-                                        <label id="${option.id}" class="section__question-option ${option.id === question.answer ? 'section__question-option_answer' : ''}">
-                                            ${option.img ? 
-                                                `<img class="section__question-img" src="${option.img}"/>` : 
-                                                `<input class="section__question-radio" type="radio" name="radio-${question.answer}"></input>${option.text}`
-                                            }
-                                        </label>
-                                    `).join('')}
-                                </div>
-                            </div>
+function toggleThemeContent(themeEl){
+    const contentEl = themeEl.querySelector('.section__theme-content');
+    if(contentEl){
+        contentEl.remove();
+        return;
+    }
+    const sectionEl = themeEl.closest('.section');
+    const questionList = theory[sectionEl.dataset.sectionIndex].themes[themeEl.dataset.themeIndex].questions;
+    const contentHTML = `
+        <div class="section__theme-content">
+            ${questionList.map((question, qIndex) => `
+                <div class="section__question" id="${question.id}">
+                    <div class="section__question-text">
+                        <div class="section__question-text-number">${qIndex + 1}.&nbsp;</div>
+                        <div>${question.text} ${question.siblings.map((sib, sInd) => `<span class="section__question-sibling" data-id="${sib}">Такой же ${sInd + 1}</span>`).join('')}</div>
+                    </div>
+                    <div class="section__question-options">
+                        ${question.options.map(option => `
+                            <label id="${option.id}" class="section__question-option ${option.id === question.answer ? 'section__question-option_answer' : ''}">
+                                ${option.img ? 
+                                    `<img class="section__question-img" src="${option.img}"/>` : 
+                                    `<input class="section__question-radio" type="radio" name="radio-${question.answer}"></input>${option.text}`
+                                }
+                            </label>
                         `).join('')}
                     </div>
+                </div>
+            `).join('')}
+        </div>`;
+    themeEl.insertAdjacentHTML('beforeend', contentHTML);
+}
+
+const contentHTML = theory.map((section, sectionIndex) => `
+    <div class="section" data-section-index="${sectionIndex}">
+        <div class="section__title">Раздел ${section.name}</div>
+        <div class="section__content">
+            ${section.themes.map((theme, themeIndex) => `
+                <div class="section__theme" data-theme-index="${themeIndex}">
+                    <div class="section__theme-title">${theme.title} <b>(${theme.questions.length})</b></div>
                 </div>
             `).join('')}
         </div>
